ad19da2ba89d236579e91012af10366392b39307
Harshkaushik38 <harshkaushik6289@gmail.com>
2024-09-09 16:00:51 +0530

    add new file

---
+++ vite-project/src/components/Github/Github.jsx
@@ -0,0 +1,36 @@
+
+import React, { useEffect, useState } from "react";
+import { useLoaderData } from "react-router-dom";
+
+function GitHub(){
+   const data = useLoaderData()
+//   useEffect(()=>{
+//   fetch('https://api.github.com/users/harshkaushik38')
+//   .then(response => response.json())
+//   .then(data => {
+//     console.log(data)
+//     setData(data)
+//   })
+  
+
+
+//   },[])
+
+
+ return(
+    <div className="text-center m-5 bg-gray-700 text-white p-4 text-3xl">GitHub followers : {data.followers}
+      <img src={data.avatar_url} alt="" width={300} />
+
+    
+    </div>
+ )
+}
+
+
+export default GitHub;
+
+
+export const githubInfoLoader = async ()=>{
+    const response = await fetch ('https://api.github.com/users/harshkaushik38')
+    return response.json()
+}
--- vite-project/src/components/Home/Home.jsx
+++ vite-project/src/components/Home/Home.jsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Home() {
     return (
@@ -44,4 +44,3 @@ export default function Home() {
         </div>
     );
 }
-
